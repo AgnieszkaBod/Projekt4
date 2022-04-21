05c1b76f6c66ceea5ac02eea623fa6eca6d06b6f
AgnieszkaB <agnieszka.bodziak@britenet.com.pl>
2022-04-21 13:07:51 +0200

    some changes

---
+++ src/main/java/DataBase.java
@@ -0,0 +1,68 @@
+import java.sql.*;
+
+public class DataBase {
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+
+
+    public void connect() {
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "password");
+            st = con.createStatement();
+
+
+        } catch (Exception ex) {
+            System.out.println("Blad polaczenia: " + ex);
+        }
+    }
+
+    public void insertData() {
+
+        String query = "INSERT INTO laptops "
+                + " (id, producent, przekatna, rozdzielczosc, matryca, dotykowy, procesor, rdzenie, taktowanie, ram," +
+                " dysk, rdysku, nazwagraf, uklgraf, system, naped) values" +
+                "(1,'Dell', '12\"', 'Brak informacji', 'matowa', 'nie', 'intel i7', '4', '2800', '8GB', '240GB', 'SSD', 'intel HD Graphics 4000', '1GB', 'Windows 7 Home', 'Brak informacji')," +
+                "(2, 'Asus', '14\"', '1600x900', 'matowa', 'nie', 'intel i5', '4', 'Brak informacji', '16GB', '120GB', 'SSD', 'intel HD Graphics 5000', '1GB', 'Brak informacji', 'brak')," +
+                "(3, 'Fujitsu', '14\"', '1920x1080', 'blyszczaca', 'tak', 'intel i7', '8', '1900', '24GB', '500GB', 'HDD', 'intel HD Graphics 520',  '1GB', 'brak systemu', 'Blu-Ray')," +
+                "(4, 'Huawei', '13\"', 'Brak informacji', 'matowa', 'nie', 'intel i7', '4', '2400', '12GB', '24GB', 'HDD', 'NVIDIA GeForce GTX 1050', 'Brak informacji', 'Brak informacji', 'brak')," +
+                "(5, 'MSI', '17\"', '1600x900', 'blyszczaca', 'tak', 'intel i7', '4', '3300', '8GB', '60GB', 'SSD', 'AMD Radeon Pro 455', '1GB', 'Windows 8.1 Profesional', 'DVD')," +
+                "(6, 'Dell', 'Brak informacji', '1280x800', 'matowa','nie', 'intel i7', '4', '2800', '8GB', '240GB', 'SSD', 'Brak informacji', 'Brak informacji', 'Windows 7 Home','brak')," +
+                "(7, 'Asus', '14\"', '1600x900', 'matowa', 'nie', 'intel i5', '4', '2800', 'Brak informacji', '120GB', 'SSD', 'intel HD Graphics 5000', '1GB', 'Windows 10 Home', 'Brak informacji')," +
+                "(8, 'Fujitsu', '15\"', '1920x1080', 'blyszczaca', 'tak', 'intel i7', '8', '2800', '24GB', '500GB', 'HDD', 'intel HD Graphics 520', 'Brak informacji', 'brak systemu', 'Blu-Ray')," +
+                "(9, 'Samsung', '13\"', '1366x768',  'matowa', 'nie', 'intel i7', '4', '2800', '12GB', '24GB' , 'HDD', 'NVIDIA GeForce GTX 1050', '1GB', 'Windows 10 Home', 'brak')," +
+                "(10, 'Sony', '16\"', 'Brak informacji', 'blyszczaca', 'tak', 'intel i7', '4', '2800', '8GB', 'Brak informacji', 'Brak informacji', 'AMD Radeon Pro 455', '1GB',  'Windows 7 Profesional', 'DVD')," +
+                "(11, 'Samsung', '12\"', '1280x800', 'matowa', 'nie', 'intel i7', 'Brak informacji', '2120', 'Brak informacji', 'Brak informacji', 'Brak informacji', 'intel HD Graphics 4000', '1GB', 'Brak informacji', 'brak')," +
+                "(12, 'Samsung', '14\"', '1600x900', 'matowa',  'nie', 'intel i5', 'Brak informacji', 'Brak informacji', 'Brak informacji', 'Brak informacji', 'SSD', 'intel HD Graphics 5000', '1GB', 'Windows 10 Home', ' brak')," +
+                "(13, 'Fujitsu', '15\"', '1920x1080', 'blyszczaca', 'tak', 'intel i7', '8', '2800', '24GB', '500GB', 'HDD', 'intel HD Graphics 520', 'Brak informacji', 'brak systemu', 'Blu-Ray')," +
+                "(14, 'Huawei', '13\"', '1366x768', 'matowa', 'nie', 'intel i7', '4', '3000', 'Brak informacji', '24GB', 'HDD', 'NVIDIA GeForce GTX 1050', 'Brak informacji', 'Windows 10 Home', 'brak')," +
+                "(15, 'MSI', '17\"', '1600x900',  'blyszczaca', 'tak', 'intel i7', '4', '9999', '8GB', '60GB', 'SSD', 'AMD Radeon Pro 455', '1GB', 'Windows 7 Profesional', 'Brak informacji')," +
+                "(16, 'Huawei', '14\"', 'Brak informacji', 'matowa', 'nie', 'intel i7', '4', '2200', '8GB', '16GB', 'HDD', 'NVIDIA GeForce GTX 1080', 'Brak informacji', 'Brak informacji', 'brak')," +
+                "(17, 'MSI', '17\"', '1600x900', 'blyszczaca',  'tak', 'intel i7', '4', '3300', '8GB', '60GB', 'SSD', 'AMD Radeon Pro 455', '1GB', 'Brak informacji', 'Brak informacji')," +
+                "(18, 'Asus', 'Brak informacji', '1600x900', 'blyszczaca', 'tak', 'intel i5', '2', '3200', '16GB', '320GB', 'HDD', 'Brak informacji', 'Brak informacji', 'Windows 7 Home', 'brak')," +
+                "(19, 'Asus', '14\"', '1600x900', 'matowa', 'nie', 'intel i5', '4', '2800', 'Brak informacji', '120GB', 'SSD', 'intel HD Graphics 5000', '1GB', 'Windows 10 Profesional', 'Brak informacji')," +
+                "(20, 'Fujitsu', '14\"', '1280x800', 'blyszczaca', 'tak', 'intel i7', '8', '2800', '24GB', '500GB', 'HDD', 'intel HD Graphics 520', 'Brak informacji', 'brak systemu', 'Blu-Ray')," +
+                "(21, 'Samsung', '12\"', '1600x900', 'Brak informacji', 'nie', 'intel i5', '4', '2800', '12GB', '24GB', 'HDD', 'NVIDIA GeForce GTX 1050', '1GB', 'Windows 8.1 Home', 'brak')," +
+                "(22, 'Sony', '11\"', 'Brak informacji', 'blyszczaca', 'tak', 'intel i7', '4', '2800', '8GB', 'Brak informacji', 'Brak informacji', 'AMD Radeon Pro 455', '1GB', 'Windows 7 Profesional', 'brak')," +
+                "(23, 'Samsung', '13\"', '1366x768', 'Brak informacji', 'nie', 'intel i5', 'Brak informacji', '2120', 'Brak informacji', 'Brak informacji', 'Brak informacji', 'intel HD Graphics 4000', '2GB', 'Brak informacji', 'DVD')," +
+                "(24, 'Samsung', '15\"', '1920x1080', 'matowa', 'nie', 'intel i9', 'Brak informacji',' Brak informacji', 'Brak informacji', 'Brak informacji', 'SSD', 'intel HD Graphics 4000', '2GB', 'Windows 10 Profesional', 'Blu-Ray');";
+
+        try {
+            st.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getData() {
+        String query = " SELECT * FROM laptops";
+        try {
+            rs = st.executeQuery(query);
+            System.out.println(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,227 @@
-import java.sql.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Main {
-    private Connection con;
-    private Statement st;
-    private ResultSet rs;
+import static java.awt.Color.*;
+import static java.awt.FlowLayout.LEFT;
+import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED;
+import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED;
 
+public class Main extends JFrame {
+    public static String[] headers = new String[15];
+    public static Object[][] data;
+    public static Object[][] dataPom;
+    public static final String filePath = "src/katalog.txt";
+    public static int baseRows = 0, pomRows = 0, columns = 15, numeberDupilacedRows = 0, numberNewRows = 0, i = 0, j = 0;
+    public static boolean isDuplicated;
+    public static List<Integer> editedRows = new ArrayList();
+    public static List<Integer> duplicatedRows = new ArrayList();
+    public static Main window = new Main();
+    public static DefaultTableModel tableModel;
 
-    public void connect() {
+    public static final JButton bImportTxt = new JButton("Importuj dane z txt");
+    public static final JButton bExportTxt = new JButton("Eksportuj dane do txt");
+
+    public static final JButton bImportXml = new JButton("Importuj dane z xml");
+    public static final JButton bExportXml = new JButton("Eksportuj dane do xml");
+
+    public static final JButton bImportDB = new JButton("Importuj dane z bazy");
+    public static final JButton bExportDB = new JButton("Eksportuj dane do bazy");
+
+    public static final JTextArea info = new JTextArea();
+
+    public Main() {
+        setSize(1500, 700);
+        setTitle("Systemy integracyjne Lab4");
+        setLayout(new FlowLayout(LEFT));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    static void getHeaders() throws IOException {
+        BufferedReader fileReader = null;
+        String line;
+
         try {
-            Class.forName("org.postgresql.Driver");
+            fileReader = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(filePath), StandardCharsets.UTF_8));
 
-            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "password");
-            st = con.createStatement();
+        } catch (
+                FileNotFoundException e) {
+            System.out.println("Blad przy otwieraniu pliku!");
+        }
 
-
-        } catch (Exception ex) {
-            System.out.println("Blad polaczenia: " + ex);
+        if (fileReader != null) {
+            line = fileReader.readLine();
+            String[] words = line.split(";");
+            int i = 0;
+            for (String word : words) {
+                headers[i] = word;
+                i++;
+            }
         }
     }
 
-    public void insertData() {
+    static void setButtons() {
+        bImportTxt.setSize(100, 100);
+        bImportTxt.setBackground(BLUE);
+        bExportTxt.setBackground(BLUE);
 
-        String query = "INSERT INTO laptops (producent, przekatna) VALUES ('intel', '14\"')";
+        bImportXml.setBackground(MAGENTA);
+        bExportXml.setBackground(MAGENTA);
+
+        bImportDB.setBackground(GREEN);
+        bExportDB.setBackground(GREEN);
+    }
+
+    static void importFromTxt() {
+        FileReader fileReader = null;
+        FileReader fileReaderPom = null;
+        String line = "";
+
+        duplicatedRows.clear();
+
         try {
-            st.executeUpdate(query);
-        } catch (SQLException e) {
+            fileReader = new FileReader(filePath);
+            fileReaderPom = new FileReader(filePath);
+        } catch (FileNotFoundException e1) {
+            System.out.println("Blad przy otwieraniu pliku!");
+        }
+
+        assert fileReader != null;
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        assert fileReaderPom != null;
+        BufferedReader bufferedReaderPom = new BufferedReader(fileReaderPom);
+        String[] words;
+
+        baseRows = 0;
+
+        try {
+            bufferedReaderPom.readLine();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        while (true) {
+            try {
+                if (null == bufferedReaderPom.readLine()) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            baseRows++;
+        }
+
+        data = new Object[baseRows][columns];
+        try {
+            line = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < baseRows; i++) {
+            try {
+                line = bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            words = line.split(";", -1);
+            for (int j = 0; j <=14; j++) {
+                data[i][j] = words[j];
+                if (data[i][j] == null || data[i][j].equals("")) {
+                    data[i][j] = "Brak informacji";
+                }
+            }
+        }
+
+        try {
+            fileReader.close();
+        } catch (IOException e) {
+            System.out.println("Blad przy zamykaniu pliku!");
+        }
+
     }
 
     public static void main(String[] args) {
-        Main db = new Main();
-        db.connect();
-        db.insertData();
+        try {
+            getHeaders();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        setButtons();
+        info.setText("Integracja systemów");
+        info.setFocusable(false);
+
+
+        tableModel = new DefaultTableModel(0, 0);     //TableModel
+        tableModel.setColumnIdentifiers(headers);
+
+        JTable table = new JTable(tableModel) {
+            @Override
+            public void setValueAt(Object aValue, int row, int column) {
+                if (aValue.toString().trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(window, "Pole nie może być puste!");
+                } else if ((column == 4 || column == 10) && aValue.toString().trim().length() != 3) {
+                    JOptionPane.showMessageDialog(window, "Tekst musi miec 3 znaki!");
+                } else if (column == 1 && !aValue.toString().endsWith("\"")) {
+                    JOptionPane.showMessageDialog(window, "Pole musi się kończyć na \"");
+                } else if (column == 2 & !aValue.toString().matches("[0-9]+x[0-9]+")) {
+                    JOptionPane.showMessageDialog(window, "Wprowadź wartość według wzoru, np. 1920x1080");
+                } else {
+                    super.setValueAt(aValue, row, column);
+
+                    if (!String.valueOf(data[row][column]).equals(String.valueOf(aValue))) {
+                        data[row][column] = aValue;
+                        editedRows.add(row);
+                    }
+                }
+            }
+
+            @Override
+            public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+                Component component = super.prepareRenderer(renderer, row, column);
+                Color color;
+
+                if (editedRows.contains(row))
+                    color = Color.WHITE;
+                else if (duplicatedRows.contains(row))
+                    color = Color.RED;
+                else
+                    color = Color.GRAY;
+
+                component.setBackground(color);
+                window.repaint();
+                return component;
+            }
+        };
+
+        JScrollPane scrollPane = new JScrollPane(table, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setPreferredSize(new Dimension(1480, 600));
+        window.add(bImportTxt);
+        window.add(bExportTxt);
+        window.add(bImportXml);
+        window.add(bExportXml);
+        window.add(bImportDB);
+        window.add(bExportDB);
+        window.add(info);
+        window.add(scrollPane);
+        window.setVisible(true);
+
+        bImportTxt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                importFromTxt();
+                info.setText("Wczytano dane z pliku txt:  " + numberNewRows + " nowych rekordow, " + numeberDupilacedRows + " duplikatow");
+                editedRows.clear();
+                window.repaint();
+            }
+        });
     }
 }
+
